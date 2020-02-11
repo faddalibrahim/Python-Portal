df741ba14267ed8bd363ae48cbc10a5ad1a419d9
Faddal <faddalibrahim@gmail.com>
2020-02-11 23:23:40 +0000

    testing ajaxxxx

--- index.js
+++ index.js
@@ -29,10 +29,25 @@ const pageTransition = debounce(() => {
     })
 
     links[index].classList.add("clicked");
-    // screens[index]
-    // console.log(screens[index]);
-    // const index = screens.scrollLeft;
-    // console.log(index);
 },60)
 
 screens_parent.addEventListener("scroll",pageTransition);
+
+
+
+document.querySelector("#screen-take-quiz").addEventListener("click", e => {
+    filename = e.target.textContent.toLowerCase().split(" ").join("-")+".html";
+
+    // alert(filename);
+
+    let http = new XMLHttpRequest();
+    http.onreadystatechange = function () {
+        if(http.readyState == 4 && http.status == 200){
+            if(this.response !== ""){
+                document.querySelector("#screen-take-quiz").innerHTML = this.responseText;
+            }
+        }
+    }
+    http.open("GET", `quiz-files/${filename}`, true);
+    http.send();
+})
