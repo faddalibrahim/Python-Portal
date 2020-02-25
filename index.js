f843210d8dfc5cea328e90a4a070dae8a1dba3b6
Faddal <faddalibrahim@gmail.com>
2020-02-25 11:23:30 +0000

    expand animation

--- index.js
+++ index.js
@@ -36,20 +36,97 @@ screens_parent.addEventListener("scroll",pageTransition);
 
 
 document.querySelector("#screen-take-quiz").addEventListener("click", e => {
-    if(e.target.className == "chapter"){
-        filename = e.target.textContent.toLowerCase().split(" ").join("-")+".html";
+    if(e.target.parentElement.className == "questionSet"){
+        let clickedChapter = e.target.parentElement.previousElementSibling.textContent.toLowerCase();
+        let clickedQuestionSet = e.target.textContent;
 
-        // alert(filename);
+        // fetch("dumb.json")
+        // .then(response => response.json())
+        // // .then( quizFile => console.log(quizFile.find(quiz => quiz.chapter == clickedChapter)))
+        // .then( quizFile => quizFile.find(quiz => quiz.chapter == clickedChapter))
+        // .then(({questionSet}) => console.log(questionSet.TrueOrFalse))
+        // .catch(err => console.log(err));
 
-        let http = new XMLHttpRequest();
-        http.onreadystatechange = function () {
-            if(http.readyState == 4 && http.status == 200){
-                if(this.response !== ""){
-                    document.querySelector("#screen-take-quiz").innerHTML = this.responseText;
-                }
-            }
-        }
-        http.open("GET", `quiz-files/${filename}`, true);
-        http.send();
+        fetch("dumb.json")
+        .then(response => response.json())
+        .then(quizFile => quizFile.find(quiz => quiz.chapter == clickedChapter))
+        // .then(({questionSet}) => render(questionSet.clickedQuestionSet))
+        .then(({questionSet}) => render(questionSet,clickedQuestionSet))
+
+        // console.log(clickedQuestionSet)
     }
-})
+    // const mainContainer = document.querySelector('#screen-take-quiz');
+
+
+    // fetch("dumb.json")
+    // .then(response => response.json())
+    // .then( ({chapter1}) =>render(chapter1, mainContainer))
+    // .catch(err => console.log(err));
+
+
+    // fetch("dumb.json")
+    // .then(response => response.json())
+    // .then( ({chapter1}) => console.log(chapter1))
+    // .catch(err => console.log(err));
+
+    // fetch("dumb.json")
+    // .then(response => response.json())
+    // .then( item => console.log(item))
+    // .catch(err => console.log(err));
+
+});
+
+
+//this function renders the selected quiz to the screen
+// const render = (chapter, mainDiv) => {
+
+// }
+
+const render = (questionSet,clickedQuestionSet) => {
+    console.log(clickedQuestionSet)
+}
+
+// const render = (chapter, mainDiv) => {
+
+
+//     const nodes = mainDiv.querySelectorAll('.chapter');
+//     for (let node of nodes) {
+//         mainDiv.removeChild(node);
+//     }
+
+//     chapter.questions.map( ({q, ans, options}, i) => {
+//         const qN = i+1;
+//     const container = document.createElement('div');
+//     const question = document.createElement('p');
+
+//     question.textContent = `${qN}. ${q}`;
+
+//     container.appendChild(question);
+
+
+//     for (let option of options) {
+//         const input = document.createElement('input');
+//         const label = document.createElement('label');
+
+
+//         input.type = "radio";
+//         input.id = `${qN}-${option}`;
+//         input.name = `${qN}-option`;
+//         input.value = option;
+
+//         label.setAttribute('for', `${qN}-${option}`);
+//         label.textContent = option.toString().toUpperCase();
+
+       
+
+//         container.appendChild(input);
+//         container.appendChild(label);
+//     }
+
+//     mainDiv.appendChild(container);
+
+
+
+//     })
+    
+// }
